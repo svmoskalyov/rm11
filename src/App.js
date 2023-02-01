9412a9746dc7265aa57c5f0e68d0da77044182e6
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-01 12:42:43 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import PaintingList from './components/PaintingList';
+import Section from './components/Section';
+import paintings from './paintings.json';
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Section title="Топ недели">
+        <PaintingList items={paintings} />
+      </Section>
+
+      <Section title="Лучшее"></Section>
+    </div>
+  );
 }
 
 export default App;
