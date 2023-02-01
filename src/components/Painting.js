92865baffbd87b2e0af5f568b0053520e9e46bd9
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-01 12:42:46 +0200

    Create Painting.js

---
+++ src/components/Painting.js
@@ -0,0 +1,25 @@
+import defaultImage from './default.jpg';
+
+const Painting = ({
+  imageUrl = defaultImage,
+  title,
+  profileUrl,
+  author = 'не известно',
+  price,
+  quantity,
+}) => {
+  return (
+    <div>
+      <img src={imageUrl} alt={title} width="480" />
+      <h2>{title}</h2>
+      <p>
+        Автор: <a href={profileUrl}>{author}</a>
+      </p>
+      <p>Цена: {price} кредитов</p>
+      <p>Доступность: {quantity < 10 ? 'заканчивается' : 'есть в наличии'}</p>
+      <button type="button">Добавить в корзину</button>
+    </div>
+  );
+};
+
+export default Painting;
