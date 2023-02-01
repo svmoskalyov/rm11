8d10e7584229b4fa00e94d1fb25f4a2c189fb2f5
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-01 12:42:48 +0200

    Create PaintingList.js

---
+++ src/components/PaintingList.js
@@ -0,0 +1,22 @@
+import Painting from './Painting';
+
+function PaintingList({ items }) {
+  return (
+    <ul>
+      {items.map(item => (
+        <li key={item.id}>
+          <Painting
+            imageUrl={item.url}
+            title={item.title}
+            author={item.author.tag}
+            profileUrl={item.author.url}
+            price={item.price}
+            quantity={item.quantity}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default PaintingList;
